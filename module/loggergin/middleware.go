219214750588596739f9e670a017de7948f192d0
therne <retail3210@gmail.com>
2019-02-13 18:10:11 +0900

    Add gin module

---
+++ module/loggergin/middleware.go
@@ -0,0 +1,36 @@
+package loggergin
+
+import (
+	"github.com/airbloc/logger"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Middleware(loggerName string) gin.HandlerFunc {
+	log := logger.New(loggerName)
+
+	return func(c *gin.Context) {
+		url := c.Request.URL.Path
+		if c.Request.URL.RawQuery != "" {
+			url = url + "?" + c.Request.URL.RawQuery
+		}
+
+		timer := log.Timer()
+		c.Next()
+
+		statusCode := c.Writer.Status()
+		statusColor := logger.Green
+		if statusCode < 200 || statusCode >= 300 {
+			statusColor = logger.Red
+		}
+		info := logger.Attrs{
+			"method": c.Request.Method,
+			"url":    url,
+			"status": statusCode,
+			"client": c.ClientIP(),
+		}
+		timer.End("{method} {url} – HTTP {}{status} {}{} – {client}",
+			statusColor, http.StatusText(statusCode), logger.Reset,
+			info)
+	}
+}
